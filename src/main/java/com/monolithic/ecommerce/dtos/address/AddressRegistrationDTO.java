d18a61e71e7e5a0dc2406e186418d5425411cb2f
georgrybski <rybskigeorg@gmail.com>
2023-04-14 08:33:04 -0300

    Added AddressRegistrationDTO

---
+++ src/main/java/com/monolithic/ecommerce/dtos/address/AddressRegistrationDTO.java
@@ -0,0 +1,18 @@
+package com.monolithic.ecommerce.dtos.address;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddressRegistrationDTO {
+    private String fullName;
+    private String phoneNumber;
+    private String street;
+    private String number;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCodeOrPostalCodeOrCEP;
+}
