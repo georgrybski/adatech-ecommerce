2d1058096b0835d1406d89743cfabeb43b883b93
georgrybski <rybskigeorg@gmail.com>
2023-04-18 14:41:01 -0300

    Added UserRepository

---
+++ src/main/java/com/monolithic/ecommerce/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.monolithic.ecommerce.repository;
+
+
+
+import com.monolithic.ecommerce.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
