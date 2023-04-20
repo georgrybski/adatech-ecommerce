67462746598a76878b6ae61f2e42277a0bae0c69
georgrybski <rybskigeorg@gmail.com>
2023-04-20 15:20:27 -0300

    Added TransactionItemCreationDTO

---
+++ src/main/java/com/monolithic/ecommerce/dtos/transaction/creation/TransactionItemCreationDTO.java
@@ -0,0 +1,17 @@
+package com.monolithic.ecommerce.dtos.transaction.creation;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class TransactionItemCreationDTO {
+    private Long bookId;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal discount;
+    private BigDecimal totalPrice;
+}
