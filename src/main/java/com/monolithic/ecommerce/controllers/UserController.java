64bc6783bc160eba6435dcfb3a491d4b0caf7ee2
georgrybski <rybskigeorg@gmail.com>
2023-04-27 15:41:32 -0300

    Added UserController

---
+++ src/main/java/com/monolithic/ecommerce/controllers/UserController.java
@@ -0,0 +1,77 @@
+package com.monolithic.ecommerce.controllers;
+
+
+import com.monolithic.ecommerce.dtos.user.UserDTO;
+import com.monolithic.ecommerce.dtos.user.UserRegistrationDTO;
+import com.monolithic.ecommerce.dtos.user.UserUpdateDTO;
+import com.monolithic.ecommerce.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UserRegistrationDTO userRegistrationDTO) {
+        userService.createUser(userRegistrationDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findUserById(id));
+    }
+
+    @GetMapping(
+            value = "/email/{email}",
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<UserDTO> getUser(@PathVariable String email) {
+        return ResponseEntity.ok(userService.findUserByEmail(email));
+    }
+
+    @PutMapping(
+            value = "/{id}",
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<Void> updateUser(@PathVariable Long id, @RequestBody UserUpdateDTO userDTO) {
+        userService.updateUser(id, userDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(
+            value = "/example",
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<UserRegistrationDTO> getUserExample() {
+        return ResponseEntity.ok(userService.getUserRegistrationDTOExample());
+    }
+}
