edae771573bbaa030293b01af9fc364d9ac31c5d
georgrybski <rybskigeorg@gmail.com>
2023-04-28 17:01:56 -0300

    Added TransactionController

---
+++ src/main/java/com/monolithic/ecommerce/controllers/TransactionController.java
@@ -0,0 +1,51 @@
+package com.monolithic.ecommerce.controllers;
+
+
+import com.monolithic.ecommerce.dtos.transaction.creation.TransactionCreationDTO;
+import com.monolithic.ecommerce.dtos.transaction.response.TransactionResponseDTO;
+import com.monolithic.ecommerce.service.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
+@AllArgsConstructor
+public class TransactionController {
+    private TransactionService transactionService;
+
+    @GetMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<TransactionResponseDTO> getTransactionById(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.fetchTransactionById(id));
+    }
+
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<TransactionResponseDTO>> getAllTransactions() {
+        return ResponseEntity.ok(transactionService.fetchAllTransactions());
+    }
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Void> createTransaction(@RequestBody TransactionCreationDTO transactionCreationDTO) {
+        transactionService.createTransaction(transactionCreationDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(
+            value = "/example",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<TransactionCreationDTO> getExampleTransaction() {
+        return ResponseEntity.ok(transactionService.getExampleTransactionCreationDTO());
+    }
+}
